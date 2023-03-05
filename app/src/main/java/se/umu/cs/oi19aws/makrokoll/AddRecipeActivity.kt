cd4d17b1bc84df0b6949a1b9180d197dc4258e1e
alexwahlroos <alex.wahlroos@hotmail.com>
2023-03-05 17:10:45 +0100

    Filter buttons design fixed

--- app/src/main/java/se/umu/cs/oi19aws/makrokoll/AddRecipeActivity.kt
+++ app/src/main/java/se/umu/cs/oi19aws/makrokoll/AddRecipeActivity.kt
@@ -1,28 +1,32 @@
 package se.umu.cs.oi19aws.makrokoll
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import se.umu.cs.oi19aws.makrokoll.databinding.ActivityAddRecipeBinding
+import kotlin.collections.ArrayList
 
 class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
+    private lateinit var binding: ActivityAddRecipeBinding
     private lateinit var nrOfServingsSpinner: Spinner
     private lateinit var measurementSpinner: Spinner
+    private var filterList = ArrayList<Button>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_recipe)
+        binding = ActivityAddRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initNrOfServingsSpinner()
         initMeasurementSpinner()
+        initFilterList()
     }
 
     private fun initNrOfServingsSpinner(){
-        nrOfServingsSpinner = findViewById(R.id.nr_of_servings_spinner)
+        nrOfServingsSpinner = binding.nrOfServingsSpinner
 
         var adapter:ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this,
             R.array.numbers, android.R.layout.simple_spinner_item)
@@ -33,7 +37,7 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
     }
 
     private fun initMeasurementSpinner(){
-        measurementSpinner = findViewById(R.id.measurement_spinner)
+        measurementSpinner = binding.measurementSpinner
 
         var adapter:ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this,
             R.array.measurements, android.R.layout.simple_spinner_item)
@@ -53,5 +57,23 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("Not yet implemented")
+    }
+
+    private fun initFilterList(){
+        filterList.add(binding.filterButtonFisk)
+        filterList.add(binding.filterButtonFlask)
+        filterList.add(binding.filterButtonFagel)
+        filterList.add(binding.filterButtonMjolk)
+        filterList.add(binding.filterButtonNot)
+        filterList.add(binding.filterButtonVeg)
+        filterList.add(binding.filterButtonVilt)
+        filterList.add(binding.filterButtonAgg)
+        filterList.add(binding.filterButtonOvrigt)
+
+        for(btn in filterList){
+            btn.setOnClickListener(){
+                btn.isActivated = !btn.isActivated
+            }
+        }
     }
 }
