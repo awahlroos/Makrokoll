47c6d43f496515dd499fd29160d55c3156e38536
alexwahlroos <alex.wahlroos@hotmail.com>
2023-03-08 08:52:34 +0100

    Actionbar and camera work in progress

--- app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/AddRecipeActivity.kt
+++ app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/AddRecipeActivity.kt
@@ -1,15 +1,23 @@
 package se.umu.cs.oi19aws.makrokoll.controllers
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.FileProvider
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import se.umu.cs.oi19aws.makrokoll.R
 import se.umu.cs.oi19aws.makrokoll.databinding.ActivityAddRecipeBinding
 import se.umu.cs.oi19aws.makrokoll.models.IngredientsModel
 import se.umu.cs.oi19aws.makrokoll.models.IngredientsRecyclerViewAdapter
+import java.io.File
 import kotlin.collections.ArrayList
 
 class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
@@ -23,10 +31,21 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
     private var nrOfServings = ""
     private var measurementUnit = ""
     private var ingredientModel = ArrayList<IngredientsModel>()
-    private var ingredientList = ArrayList<String>()
+
+    private lateinit var file: File
+    private lateinit var viewModel: ImageViewModel
+
+    val launcher=registerForActivityResult(
+        //Vi använder det färdiga kontraktet för att ta bilder
+        ActivityResultContracts.TakePicture()) {
+        if(it) {
+            viewModel.file= File(filesDir, "mypic.jpg") //Uppdatera bilden
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ImageViewModel::class.java]
         binding = ActivityAddRecipeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -35,7 +54,50 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
         setAddIngredientListener()
         initFilterList()
         setPublishListener()
+
+        viewModel.bitmap.observe(this) {
+            binding.postedImageIV.setImageBitmap(it)
+        }
+
+        //Plats att spara bilden
+        file=File(filesDir, "mypic.jpg")
+        if(file.exists()&&viewModel.file==null) {
+            viewModel.file=file
+        }
+
+        //Sätt en lyssnare för knappen
+        binding.addImageButton.setOnClickListener { takePicture() }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.actionbar, menu)
+        val search = menu.findItem(R.id.search)
+        val searchView = search.actionView as SearchView
+
+        //Set listener to handle searches from the searchView in the actionbar
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //TODO: Implementera med databasen
+                //model.search(query)
+                //setListData()
+                search.collapseActionView()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+
 
     private fun initNrOfServingsSpinner(){
         nrOfServingsSpinner = binding.nrOfServingsSpinner
@@ -77,17 +139,24 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
         val addIngredientButton = binding.addIngredientButton
 
         addIngredientButton.setOnClickListener {
-            val weightVolume = binding.weightVolumeET.text.toString()
-            val ingredientName = binding.ingredientNameET.text.toString().lowercase()
+            if(binding.weightVolumeET.text.toString() != "") {
+                if(binding.ingredientNameET.text.toString() != "") {
+                    val weightVolume = binding.weightVolumeET.text.toString()
+                    val ingredientName = binding.ingredientNameET.text.toString().lowercase()
 
-            recyclerView = binding.recyclerView
-            updateIngredientModel(weightVolume, measurementUnit, ingredientName)
-            val adapter = IngredientsRecyclerViewAdapter(this, ingredientModel)
-            recyclerView.adapter = adapter
-            recyclerView.layoutManager = LinearLayoutManager(this)
+                    recyclerView = binding.recyclerView
+                    updateIngredientModel(weightVolume, measurementUnit, ingredientName)
+                    val adapter = IngredientsRecyclerViewAdapter(this, ingredientModel)
+                    recyclerView.adapter = adapter
+                    recyclerView.layoutManager = LinearLayoutManager(this)
 
-            binding.weightVolumeET.text.clear()
-            binding.ingredientNameET.text.clear()
+                    binding.weightVolumeET.text.clear()
+                    binding.ingredientNameET.text.clear()
+                    binding.weightVolumeET.requestFocus()
+                } else Toast.makeText(this,"Ange namn på ingrediens", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Ange vikt/volym", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -104,6 +173,7 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
 
         for(btn in filterList){
             btn.setOnClickListener {
+                //TODO: Spara undan tillstånd
                 //TODO: Fixa så bara ett alternativ kan vara aktivt??
                 //TODO: Alternativt: fixa ikon för flera val av protein
                 btn.isActivated = !btn.isActivated
@@ -116,13 +186,21 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
         val activeFiltersList = ArrayList<String>()
 
         publishButton.setOnClickListener {
-            val nameOfRecipe = binding.recipeNameTV.text.toString()
-            //nrOfServings from spinner
             for(btn in filterList){
                 if(btn.isActivated) {
                     //Store only the filter phrase, ie. "Beef" or "Egg"
                     activeFiltersList.add(resources.getResourceEntryName(btn.id).substring(12))
                 }
+            }
+
+            if(binding.recipeNameET.text.toString() != "") {
+                if(activeFiltersList.isNotEmpty()) {
+                    //Hämta nrOfServings, lagrad i variabeln redan
+                    val nameOfRecipe = binding.recipeNameET.text.toString()
+                } else Toast.makeText(this, "Proteinkälla saknas", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Namn på recept saknas", Toast.LENGTH_LONG).show()
+                binding.recipeNameET.requestFocus()
             }
         }
     }
@@ -131,5 +209,33 @@ class AddRecipeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
                                       measurementUnit:String,
                                       ingredientName:String) {
         ingredientModel.add(IngredientsModel(weightVolume, measurementUnit, ingredientName))
+    }
+
+
+    //Camera
+    private fun takePicture() {
+        /* Bestäm vart filen ska sparas
+         * Observera att Kamera-appen måste kunna skriva till platsen
+         * där filen finns därav används en FileProvider
+         */
+        val uri: Uri
+
+        //Spara filen i vår local
+        // storage med en content-url. Det låter de oss spara filen var som helst utan att behöva
+        // bry os om att kameraappen har rättighet at skriva dit. Dessutom har vi
+        // om vi sparar filen på ett ställe vi har koll på kontroll över att ingen
+        // annan app sabbar något
+        uri = FileProvider.getUriForFile(applicationContext, "$packageName.fileprovider", file)
+
+        //Starta aktiviteten
+        launcher.launch(uri)
+    }
+
+    /*
+     * Se till så att vi uppdaterar om storleken på vyn skulle ändras (tex pga rotation).
+     */
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        viewModel.setSize(binding.postedImageIV.height,binding.postedImageIV.width)
     }
 }
