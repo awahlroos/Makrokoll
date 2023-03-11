4074a896751937d26d77518952322dbbd080b32f
alexwahlroos <alex.wahlroos@hotmail.com>
2023-03-11 10:09:35 +0100

    Restructure of code

--- app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/FabFragment.kt
+++ app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/FabFragment.kt
@@ -1,13 +1,13 @@
-package se.umu.cs.oi19aws.makrokoll
+package se.umu.cs.oi19aws.makrokoll.controllers
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.navigation.Navigation
+import se.umu.cs.oi19aws.makrokoll.R
 
 class FabFragment : Fragment() {
 
