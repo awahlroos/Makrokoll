c901e66bf96f385fd737d76bd842b970d56cc360
alexwahlroos <alex.wahlroos@hotmail.com>
2023-03-11 10:07:41 +0100

    Restructure of code

--- app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/ProfileFragment.kt
+++ app/src/main/java/se/umu/cs/oi19aws/makrokoll/controllers/ProfileFragment.kt
@@ -1,10 +1,11 @@
-package se.umu.cs.oi19aws.makrokoll
+package se.umu.cs.oi19aws.makrokoll.controllers
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import se.umu.cs.oi19aws.makrokoll.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [discover.newInstance] factory method to
+ * Use the [profile.newInstance] factory method to
  * create an instance of this fragment.
  */
-class discover : Fragment() {
+class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class discover : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class discover : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment discover.
+         * @return A new instance of fragment profile.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            discover().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
